d84a4a755a74553d3add2852ecec9c6f8f0abad5
siekiera92 <piotr.siekierski.92@gmail.com>
2017-03-27 16:01:44 +0200

    początek zadania

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -45,3 +45,15 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
 
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+};
+
+function onError() {
+    alert('onError!');
+};
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
